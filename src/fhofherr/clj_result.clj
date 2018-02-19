089b5a76ac0744d61e58315fe12dcf33b50c87a3
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-02-19 12:02:22 +0100

    Add specs

--- src/fhofherr/clj_result.clj
+++ src/fhofherr/clj_result.clj
@@ -1,9 +1,18 @@
-(ns fhofherr.clj-result)
+(ns fhofherr.clj-result
+  (:require [clojure.spec.alpha :as s]
+            [clojure.core.specs.alpha :as cs]))
 
 (defprotocol ^:no-doc CljResult
   (-error? [this] "Check if the result is an error.")
   (-success? [this] "Check if the result is an success.")
   (-value [this] "Get the results value."))
+
+(defn result?
+  "Check if `x` is a result."
+  [x]
+  (satisfies? CljResult x))
+
+(s/def ::clj-result result?)
 
 (defrecord ^:private CljError [value]
   CljResult
@@ -22,25 +31,45 @@
   [result]
   (-error? result))
 
+(s/fdef error?
+        :args (s/cat :result ::clj-result)
+        :ret boolean?)
+
 (defn success?
   "Check if the result is a success"
   [result]
   (-success? result))
+
+(s/fdef success?
+        :args (s/cat :result ::clj-result)
+        :ret boolean?)
 
 (defn value
   "Get the result value"
   [result]
   (-value result))
 
+(s/fdef value
+        :args (s/cat :result ::clj-result)
+        :ret any?)
+
 (defn error
   "Turn `value` into an error."
   [value]
   (->CljError value))
 
+(s/fdef error
+        :args (s/cat :value any?)
+        :ret (s/and ::clj-result error?))
+
 (defn success
   "Turn `value` into a success."
   [value]
   (->CljSuccess value))
+
+(s/fdef success
+        :args (s/cat :value any?)
+        :ret (s/and ::clj-result success?))
 
 (defn m-bind
   "Apply the function `f` to `result` if `result` is a success. Do nothing
@@ -53,9 +82,17 @@
   where `v` is the value of the result passed to `m-bind`, `R` is a new result
   with a potentially updated value `w`."
   [result f]
+  {:pre [(result? result) (fn? f)]
+   :post [#(result? %)]}
   (if (-success? result)
     (-> result value f)
     result))
+
+(s/fdef m-bind
+        :args (s/cat :result ::clj-result
+                     :f (s/fspec :args (s/cat :v any?)
+                                 :ret ::clj-result))
+        :ret ::clj-result)
 
 (defn- swap-pairs
   [xs]
@@ -78,11 +115,19 @@
   (let [swapped (swap-pairs bindings)]
     (emit-m-bind swapped body)))
 
+(s/fdef attempt
+        :args (s/cat :bindings ::cs/bindings
+                     :body any?))
+
 (defmacro attempt-v
   "Attempt all given operations just like [[attempt]] but return the value
   of the success or error."
   [bindings & body]
   `(value (attempt ~bindings ~@body)))
+
+(s/fdef attempt-v
+        :args (s/cat :bindings ::cs/bindings
+                     :body any?))
 
 (defmacro attempt-as->
   "Binds `init-result` to `sym`. If `init-result` is a success it evaluates the
@@ -91,8 +136,18 @@
   [init-result sym & forms]
   `(attempt [~sym ~init-result ~@(mapcat #(vector sym %) forms)] ~sym))
 
+(s/fdef attempt-as->
+        :args (s/cat :init-result any?
+                     :sym ::cs/local-name
+                     :forms (s/* any?)))
+
 (defmacro attempt-v-as->
   "Just like [[attempt-as->]] but returns the value of the last encountered
   success or the first encountered error."
   [init-result sym & forms]
   `(value (attempt-as-> ~init-result ~sym ~@forms)))
+
+(s/fdef attempt-v-as->
+        :args (s/cat :init-result any?
+                     :sym ::cs/local-name
+                     :forms (s/* any?)))
