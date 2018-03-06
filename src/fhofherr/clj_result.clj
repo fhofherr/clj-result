b50301d512c7661a3feb219820e0dfc872ef1d73
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-03-06 09:21:03 +0100

    Remove CljSuccess

--- src/fhofherr/clj_result.clj
+++ src/fhofherr/clj_result.clj
@@ -3,8 +3,6 @@
             [clojure.core.specs.alpha :as cs]))
 
 (defprotocol ^:no-doc CljResult
-  (-error? [this] "Check if the result is an error.")
-  (-success? [this] "Check if the result is an success.")
   (-value [this] "Get the results value.")
   (-update-value [this f] "Apply the function f to the results value"))
 
@@ -17,35 +15,8 @@
 
 (defrecord ^:private CljError [value]
   CljResult
-  (-error? [_] true)
-  (-success? [_] false)
   (-value [this] (:value this))
   (-update-value [this f] (-> value f ->CljError)))
-
-(defrecord ^:private CljSuccess [value]
-  CljResult
-  (-error? [_] false)
-  (-success? [_] true)
-  (-value [this] (:value this))
-  (-update-value [this f] (-> value f ->CljSuccess)))
-
-(defn error?
-  "Check if the result is an error"
-  [result]
-  (-error? result))
-
-(s/fdef error?
-        :args (s/cat :result ::clj-result)
-        :ret boolean?)
-
-(defn success?
-  "Check if the result is a success"
-  [result]
-  (-success? result))
-
-(s/fdef success?
-        :args (s/cat :result ::clj-result)
-        :ret boolean?)
 
 (defn value
   "Get the result value"
@@ -54,31 +25,10 @@
     (-value result)
     result))
 
-(s/fdef value
-        :args (s/cat :result ::clj-result)
-        :ret any?)
-
 (defn error
   "Turn `value` into an error."
   [value]
   (->CljError value))
-
-(s/fdef error
-        :args (s/cat :value any?)
-        :ret (s/and ::clj-result error?))
-
-(defn success
-  "Turn `value` into a success."
-  [value]
-  (cond
-    (not (result? value)) (->CljSuccess value)
-    (error? value) (throw (IllegalArgumentException.
-                            "Can't turn an error into a success"))
-    :else value))
-
-(s/fdef success
-        :args (s/cat :value any?)
-        :ret (s/and ::clj-result success?))
 
 (defn m-bind
   "Apply the function `f` to `v` if `v` is not an error. Do nothing
@@ -92,18 +42,9 @@
   [v f]
   {:pre [(fn? f)]
    :post [#(result? %)]}
-  (if (and (result? v) (error? v))
+  (if (result? v)
     v
-    (let [next-result (-> v success value f)]
-      (if (result? next-result)
-        next-result
-        (success next-result)))))
-
-(s/fdef m-bind
-        :args (s/cat :result ::clj-result
-                     :f (s/fspec :args (s/cat :v any?)
-                                 :ret ::clj-result))
-        :ret ::clj-result)
+    (-> v value f)))
 
 (defn- swap-pairs
   [xs]
@@ -116,12 +57,12 @@
   [[r s & xs] body]
   (if xs
     `(m-bind ~r (fn [~s] ~(emit-m-bind xs body)))
-    `(m-bind ~r (fn [~s] (success ~@body)))))
+    `(m-bind ~r (fn [~s] ~@body))))
 
 (defmacro attempt
   "Attempt all given operations and bind the values to the respective symbols.
-  Return the result of evaluating `body` as a `success`. Abort immediately if
-  one operations returns an error and return the error instead of the body."
+  Return the result of evaluating `body`. Abort immediately if one operation
+  returns an error and return the error instead of the body."
   [bindings & body]
   (let [swapped (swap-pairs bindings)]
     (emit-m-bind swapped body)))
@@ -131,8 +72,8 @@
                      :body any?))
 
 (defmacro attempt-as->
-  "Binds `init-result` to `sym`. If `init-result` is a success it evaluates the
-  forms until it exhausted all or one evaluated to an error.  Returns the
+  "Binds `init-result` to `sym`. If `init-result` is not an error it evaluates
+  the forms until it exhausted all or one evaluated to an error. Returns the
   result of the last form, or the first encountered error."
   [init-result sym & forms]
   `(attempt [~sym ~init-result ~@(mapcat #(vector sym %) forms)] ~sym))
@@ -146,38 +87,21 @@
   "Apply the function `f` to the value wrapped by `result`. Return a result
   of the same kind."
   [f result]
-  (-update-value result f))
-
-(s/fdef map-v
-        :args (s/cat :result ::clj-result
-                    :f (s/fspec :args (s/cat :x any?)
-                                :ret any?))
-        :ret ::clj-result)
+  (if (result? result)
+    (-update-value result f)
+    (f result)))
 
 (defn map-e
   "Apply the function `f` to value wrapped by `result` if `result`
   is an error. Return an error with the updated value."
   [f result]
-  (if (error? result)
+  (if (result? result)
     (map-v f result)
     result))
-
-(s/fdef map-e
-        :args (s/cat :result ::clj-result
-                    :f (s/fspec :args (s/cat :x any?)
-                                :ret any?))
-        :ret ::clj-result)
 
 (defn map-s
-  "Apply the function `f` to value wrapped by `result` if `result`
-  is a success Return a success with the updated value."
+  "Apply the function `f` to `v` if `v` is not an error."
   [f result]
-  (if (success? result)
+  (if ((complement result?) result)
     (map-v f result)
     result))
-
-(s/fdef map-s
-        :args (s/cat :result ::clj-result
-                    :f (s/fspec :args (s/cat :x any?)
-                                :ret any?))
-        :ret ::clj-result)
--- test/fhofherr/clj_result_test.clj
+++ test/fhofherr/clj_result_test.clj
@@ -5,55 +5,38 @@
 (deftest error-implments-result
   (let [value "Ooops!"
         err (result/error value)]
-    (is (true? (result/error? err)))
-    (is (false? (result/success? err)))
+    (is (true? (result/result? err)))
     (is (= value (result/value err)))))
-
-(deftest success-implments-result
-  (let [value "Yeah!"
-        success (result/success value)]
-    (is (false? (result/error? success)))
-    (is (true? (result/success? success)))
-    (is (= value (result/value success)))))
 
 (deftest m-bind
 
-  (testing "apply function to success"
-    (let [init-success (result/success 1)
-          f (fn [v] (result/success (+ 1 v)))
-          res (result/m-bind init-success f)]
-      (is (result/success? res))
-      (is (= 2 (result/value res)))))
+  (testing "apply function to value"
+    (let [init-value 1
+          f (fn [v] (+ 1 v))
+          res (result/m-bind init-value f)]
+      (is (= 2 res))))
 
-  (testing "auto-wrap values into success"
-    (let [f #(+ 1 %)
-          res (result/m-bind 1 f)]
-      (is (= 2 (result/value res)))))
+  (testing "left identity"
+    (let [f (fn [v] (+ 1 v))]
+      (is (= (result/m-bind 1 f) (f 1)))))
 
-  (testing "left identity for success"
-    (let [f (fn [v] (result/success (+ 1 v)))]
-      (is (= (result/m-bind (result/success 1) f)
-            (f 1)))))
-
-  (testing "right identity for success"
-    (let [s (result/success 1)
-          f result/success]
+  (testing "right identity"
+    (let [s 1
+          f identity]
       (is (= s (result/m-bind s f)))))
 
   (testing "associativity"
-    (let [s (result/success 1)
-          f (fn [v] (result/success (+ 1 v)))
-          g (fn [v] (result/success (+ 10 v)))]
-      (is (= (result/m-bind (result/m-bind s f)
-                            g)
-             (result/m-bind s
-                            (fn [v] (result/m-bind (f v) g)))))))
+    (let [s 1
+          f (fn [v] (+ 1 v))
+          g (fn [v] (+ 10 v))]
+      (is (= (result/m-bind (result/m-bind s f) g)
+             (result/m-bind s (fn [v] (result/m-bind (f v) g)))))))
 
   (testing "do not apply function to error"
     (let [init-error (result/error 1)
-          f (fn [v] (result/success (+ 1 v)))
+          f (fn [v] (+ 1 v))
           res (result/m-bind init-error f)]
-      (is (result/error? res))
+      (is (result/result? res))
       (is (= 1 (result/value res))))))
 
 (deftest swap-pairs
@@ -64,51 +47,49 @@
 (deftest attempt
 
   (testing "single binding"
-    (is (= (result/success :a)
-           (result/attempt [x (result/success :a)]
-                           x))))
+    (is (= :a (result/attempt [x :a] x))))
 
   (testing "multiple bindings"
-    (is (= (result/success :b)
-           (result/attempt [x (result/success :a)
-                            y (result/success :b)]
+    (is (= :b
+           (result/attempt [x :a
+                            y :b]
                            y))))
 
   (testing "abort on error"
     (is (= (result/error :error)
            (result/attempt [x (result/error :error)
-                            y (result/success :a)]
+                            y :a]
                            y)))))
 
 (deftest attempt-as->
 
-  (testing "evaluate to last success"
-    (is (= (result/success :a)
-           (result/attempt-as-> (result/success :a) $)))
-    (is (= (result/success 2)
-           (result/attempt-as-> (result/success 1) $
-                                (result/success (+ 1 $))))))
+  (testing "evaluate to last value"
+    (is (= :a
+           (result/attempt-as-> :a $)))
+    (is (= 2
+           (result/attempt-as-> 1 $
+                                (+ 1 $)))))
 
   (testing "evaluate to first error"
     (is (= (result/error :a)
            (result/attempt-as-> (result/error :a) $)))
     (is (= (result/error :error)
-           (result/attempt-as-> (result/success :a) $
+           (result/attempt-as-> :a $
                                 (result/error :error)
-                                (result/success :b))))))
+                                :b)))))
 
 (deftest map-e-map-s-and-map-v
   (let [error (result/error 1)
-        success (result/success 1)]
+        v 1]
 
     (testing "map-v applies a function to the results value"
       (is (= 2 (->> error (result/map-v inc) result/value)))
-      (is (= 2 (->> success (result/map-v inc) result/value))))
+      (is (= 2 (->> v (result/map-v inc) result/value))))
 
     (testing "map-e applies a funtion to errors only"
       (is (= 2 (->> error (result/map-e inc) result/value)))
-      (is (= 1 (->> success (result/map-e inc) result/value))))
+      (is (= 1 (->> v (result/map-e inc) result/value))))
 
-    (testing "map-s applies a funtion to successes only"
+    (testing "map-s applies a funtion to non-errors only"
       (is (= 1 (->> error (result/map-s inc) result/value)))
-      (is (= 2 (->> success (result/map-s inc) result/value))))))
+      (is (= 2 (->> v (result/map-s inc) result/value))))))
